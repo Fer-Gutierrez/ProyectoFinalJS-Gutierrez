fd8de22b3671226f912c1d05e780995d4b4b520a
Fernando Gutierrez <fernandoezequiel333@gmail.com>
2023-03-17 21:45:06 -0300

    Fectch en movilidad

--- miarchivo_mov.js
+++ miarchivo_mov.js
@@ -4,52 +4,69 @@ class Movilidad {
     (this.marca = marca), (this.sumaAsegurada = sumaAsegurada);
   }
 
-  obtenerTasa() {
-    let movEncontrada = listaMarcasMovilidad.find(
-      valor => valor.marca.toLowerCase() === this.marca.toLowerCase()
-    );
-    return movEncontrada.tasaBasica;
+  async obtenerTasa() {
+    let tasa = await listaMarcasMovilidad
+                .then(data=> {
+                    let movEncontrada = data.find(valor=> valor.marca.toLowerCase() === this.marca.toLowerCase())
+                    return movEncontrada.tasaBasica;
+                })
+                .catch(error => {
+                  console.error(error)
+                });
+
+    return tasa;
   }
 }
 
 //LISTAS
-const listaMarcasMovilidad = [
-  { id: 1, marca: "SLP", tasaBasica: 35 },
-  { id: 2, marca: "Venzo", tasaBasica: 40 },
-  { id: 3, marca: "Giant", tasaBasica: 38 },
-  { id: 4, marca: "Delma", tasaBasica: 32 },
-];
-const listaPlanesMovilidad = [
-  { id: 1, nombre: "Robo Básico", factorRecargo: 1 },
-  { id: 2, nombre: "Robo y Daño", factorRecargo: 1.15 },
-  { id: 3, nombre: "Todo Riesgo", factorRecargo: 1.4 },
-];
-const listaCoberturasMovilidad = [
-  { id: 1, cobertura: "Robo Total medio de movilidad", planes: [1, 2, 3] },
-  { id: 2, cobertura: "Daños por la tentativa de robo", planes: [1, 2, 3] },
-  { id: 3, cobertura: "Hurto en la vivienda del asegurado", planes: [1, 2, 3] },
-  { id: 4, cobertura: "Daño total", planes: [2, 3] },
-  { id: 5, cobertura: "Robo de accesorios", planes: [2, 3] },
-  { id: 6, cobertura: "Robo de documentos personales", planes: [2, 3] },
-  {
-    id: 7,
-    cobertura: "Daños a equipos electrónicos (hasta $ 20.000)",
-    planes: [3],
-  },
-  {
-    id: 8,
-    cobertura: "Responsabilidad Civil por el uso de movilidad",
-    planes: [3],
-  },
-  { id: 9, cobertura: "Asistencia a la movilidad", planes: [1, 2, 3] },
-];
+
+//  const listaPlanesMovilidad = [
+//    { id: 1, nombre: "Robo Básico", factorRecargo: 1 },
+//    { id: 2, nombre: "Robo y Daño", factorRecargo: 1.15 },
+//    { id: 3, nombre: "Todo Riesgo", factorRecargo: 1.4 },
+//  ];
+//  const listaCoberturasMovilidad = [
+//    { id: 1, cobertura: "Robo Total medio de movilidad", planes: [1, 2, 3] },
+//    { id: 2, cobertura: "Daños por la tentativa de robo", planes: [1, 2, 3] },
+//    { id: 3, cobertura: "Hurto en la vivienda del asegurado", planes: [1, 2, 3] },
+//    { id: 4, cobertura: "Daño total", planes: [2, 3] },
+//    { id: 5, cobertura: "Robo de accesorios", planes: [2, 3] },
+//    { id: 6, cobertura: "Robo de documentos personales", planes: [2, 3] },
+//    {
+//      id: 7,
+//      cobertura: "Daños a equipos electrónicos (hasta $ 20.000)",
+//      planes: [3],
+//    },
+//    {
+//      id: 8,
+//      cobertura: "Responsabilidad Civil por el uso de movilidad",
+//      planes: [3],
+//    },
+//    { id: 9, cobertura: "Asistencia a la movilidad", planes: [1, 2, 3] },
+//  ];
+
+ const listaMarcasMovilidad = fetch(`./json/listaMarcasMovilidad.json`)
+ .then(response=> response.json())
+ .catch(error => console.error(error));
+
+ const listaPlanesMovilidad = fetch(`./json/listaPlanesMovilidad.json`)
+ .then(response => response.json())
+ .catch(error => console.error(error));
+
+ const listaCoberturasMovilidad = fetch(`./json/listaCoberturasMovilidad.json`)
+ .then(response => response.json())
+ .catch(error => console.error(error));
 
 //SELECT MARCA
 const selectMarca = document.querySelector("#marcaMov");
 selectMarca.innerHTML = `<option selected="true" disabled="disabled">Seleccione una marca</option>`;
-for (let marcaMov of listaMarcasMovilidad) {
-  selectMarca.innerHTML += `<option value="${marcaMov.id}">${marcaMov.marca}</option>`;
-}
+listaMarcasMovilidad
+  .then(data=>{
+    data.forEach(marcaMov =>{
+      selectMarca.innerHTML += `<option value="${marcaMov.id}">${marcaMov.marca}</option>`;
+    })
+  })
+  .catch(error=> console.error(error));
 
 //SELECT SUMA
 const inputSumaMov = document.querySelector("#sumaMov");
@@ -68,7 +85,7 @@ function cambiarFormatoSuma() {
 //SUBMIT COTIZAR
 const formCotizar = document.querySelector("#form-cotizar");
 formCotizar.addEventListener("submit", cotizar);
-function cotizar(e) {
+async function cotizar(e) {
   e.preventDefault();
   let marcaMov = selectMarca.options[selectMarca.selectedIndex].text;
   let sumaMov = quitarFormatoMoneda(inputSumaMov.value);
@@ -80,7 +97,7 @@ function cotizar(e) {
   } else {
     //Cotizamos
     let movilidad = new Movilidad(marcaMov, sumaMov);
-    let tasa = movilidad.obtenerTasa();
+    let tasa = await movilidad.obtenerTasa();
     let costoSeguro = (sumaMov * tasa) / 1000;
 
     /** RENDERIZAR **/
@@ -119,71 +136,84 @@ function cotizar(e) {
     let encabezadoTabla = document.createElement("thead");
     let filaEncabezado = document.createElement("tr");
     filaEncabezado.innerHTML = `<th scope="col">Coberturas</th>`;
-    listaPlanesMovilidad.forEach(
-      plan =>
-        (filaEncabezado.innerHTML += `<th scope="col">${plan.nombre}</th>`)
-    );
-    encabezadoTabla.append(filaEncabezado);
-    tabla.append(encabezadoTabla);
+    listaPlanesMovilidad.then(data => {
+      data.forEach(plan => (filaEncabezado.innerHTML += `<th scope="col">${plan.nombre}</th>`));
+      encabezadoTabla.append(filaEncabezado);
+      tabla.append(encabezadoTabla);
+    })
+    .catch(error => console.error(error));
 
     //CuerpoTabla
     let cuerpoTabla = document.createElement("tbody");
     cuerpoTabla.className = "table-group-divider";
-    listaCoberturasMovilidad.forEach(cobertura => {
-      let fila = document.createElement("tr");
-      let columnaCob = document.createElement("td");
-      columnaCob.innerText = cobertura.cobertura;
-      fila.append(columnaCob);
+    listaCoberturasMovilidad
+      .then(data =>{
+        data.forEach(cobertura => {
+          let fila = document.createElement("tr");
+          let columnaCob = document.createElement("td");
+          columnaCob.innerText = cobertura.cobertura;
+          fila.append(columnaCob);
+  
+          listaPlanesMovilidad.then(data => {
+            data.forEach(plan => {
+              let colummaPlan = document.createElement("td");
+              let planEncontrado = cobertura.planes.some(valor => valor === plan.id);
+              planEncontrado ? colummaPlan.innerHTML = `<i class="icon-check">`:colummaPlan.innerHTML = `<i class="icon-x">`;
+              fila.append(colummaPlan);
+            });
+          })
+          .then()
+          .catch(error=>console.error(error));
+          
+          cuerpoTabla.append(fila);
+        })
+      
+      //Fila Costo:
+      let filaCosto = document.createElement("tr");
+      filaCosto.className = "fila-costo";
+      filaCosto.innerHTML = `<td>Costo</td>`;
 
-      listaPlanesMovilidad.forEach(plan => {
-        let colummaPlan = document.createElement("td");
-        let planEncontrado = cobertura.planes.some(valor => valor === plan.id);
-        if (planEncontrado) {
-          colummaPlan.innerHTML = `<i class="icon-check">`;
-        } else {
-          colummaPlan.innerHTML = `<i class="icon-x">`;
-        }
-        fila.append(colummaPlan);
-      });
-      cuerpoTabla.append(fila);
-    });
+      //Fila Boton Contratar:
+      let filaBoton = document.createElement("tr");
+      filaBoton.className = "fila-boton";
+      filaBoton.innerHTML = "<td></td>";
 
-    //Fila Costo:
-    let filaCosto = document.createElement("tr");
-    filaCosto.className = "fila-costo";
-    filaCosto.innerHTML = `<td>Costo</td>`;
+      //Rellenado de filaCosto y filaBoton
+      listaPlanesMovilidad.then(data =>{
+        data.forEach(plan => {
+          filaCosto.innerHTML += `<td>${formatoMoneda(Math.ceil(costoSeguro * plan.factorRecargo))}</td>`;
+          filaBoton.innerHTML += `<td><button id="btnContratar${plan.id}">Contratar</button></td>`;
+        });
+      })
+      .catch(error => console.error(error))
 
-    //Fila Boton Contratar:
-    let filaBoton = document.createElement("tr");
-    filaBoton.className = "fila-boton";
-    filaBoton.innerHTML = "<td></td>";
+      //Agregamos elementos al HTML
+      cuerpoTabla.append(filaCosto);
+      cuerpoTabla.append(filaBoton);
+      tabla.append(cuerpoTabla);
+      divTabla.append(tabla);
+      div.append(divTabla);
+      const seccionCotizador = document.querySelector("#cotizador");
+      seccionCotizador.append(div);
+      formCotizar.reset();
 
-    //Rellenado de filaCosto y filaBoton
-    listaPlanesMovilidad.forEach(plan => {
-      filaCosto.innerHTML += `<td>${formatoMoneda(
-        Math.ceil(costoSeguro * plan.factorRecargo)
-      )}</td>`;
-      filaBoton.innerHTML += `<td><button id="btnContratar${plan.id}">Contratar</button></td>`;
-    });
+      //AgregadO Evento a Boton Contratar
+      listaPlanesMovilidad.then(data =>{
+        data.forEach(plan => {
+          let boton = document.querySelector(`#btnContratar${plan.id}`);
+          boton.addEventListener("click", () =>
+            agregarCotizacion(plan.id, plan.nombre, plan.factorRecargo)
+          );
+        }); 
+      })
+      .catch(error => console.log(error));
 
-    //Agregamos elementos al HTML
-    cuerpoTabla.append(filaCosto);
-    cuerpoTabla.append(filaBoton);
-    tabla.append(cuerpoTabla);
-    divTabla.append(tabla);
-    div.append(divTabla);
-    const seccionCotizador = document.querySelector("#cotizador");
-    seccionCotizador.append(div);
-    formCotizar.reset();
+      //ViewPort hacia Seccion Planes
+      let positionDivCotizacion = div.getBoundingClientRect();
+      window.scrollTo(0, window.scrollY + positionDivCotizacion.top - 50);
 
-    //AgregadO Evento a Boton Contratar
-    listaPlanesMovilidad.forEach(plan => {
-      let boton = document.querySelector(`#btnContratar${plan.id}`);
-      boton.addEventListener("click", () =>
-        agregarCotizacion(plan.id, plan.nombre, plan.factorRecargo)
-      );
-    });
-
+    })
+        
     function agregarCotizacion(idPlan, nombrePlan, recargoPlan) {
       let cotizacion = new Cotizacion(
         "movilidad",
@@ -195,9 +225,7 @@ function cotizar(e) {
       cotizacion.agregarLS();
     }
 
-    //ViewPort hacia Seccion Planes
-    let positionDivCotizacion = div.getBoundingClientRect();
-    window.scrollTo(0, window.scrollY + positionDivCotizacion.top - 50);
+    
   }
 }
 
